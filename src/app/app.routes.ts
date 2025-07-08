e911876a6226ac2d57c7f87765e6ebac4451014d
charissaida <charis.saida@gmail.com>
2025-07-08 09:29:22 +0700

    add: project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LeaveComponent } from './pages/leave/leave.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: 'employee',
+        component: EmployeeComponent,
+      },
+      {
+        path: 'leaves',
+        component: LeaveComponent,
+      },
+    ],
+  },
+];
---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  constructor(private http: HttpClient) {}
+
+  onLogin(obj: any) {
+    return this.http.post(
+      'https://freeapi.miniprojectideas.com/api/EmployeeLeave/Login',
+      obj
+    );
+  }
+}
